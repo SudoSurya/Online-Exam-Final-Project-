1817262d680dcfc452697ebf6aeb2e91aa8d9db6
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-03-12 20:50:42 +0530

    Exam results Routes Created

---
+++ Backend/Routes/ExamResult.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const StudentResults = require("../Schemas/StudentResults");
+let router = express.Router();
+router.use(express.json());
+
+router.patch("/submit/result/:id", async (req, res) => {
+  const userID = req.params.id;
+  const newResult = req.body.Results[0];
+  try {
+    const student = await StudentResults.find({ userID: userID });
+
+    if (student.length === 0) {
+      return res.status(404).json({ message: "Faculty member not found" });
+    }
+
+    student[0].Results.push(newResult);
+    const updatedStudent = await student[0].save();
+
+    res.json(updatedStudent);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.get("/result/:id", async (req, res) => {
+  const userID = req.params.id;
+  try {
+    const student = await StudentResults.find({ userID: userID });
+
+    if (student.length === 0) {
+      return res.status(404).json({ message: "Faculty member not found" });
+    }
+
+    res.json(student[0].Results);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+module.exports = router;
--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const multer = require("multer");
-const csvParser = require("csv-parser");
 const UserRoutes = require("./Routes/UserRoutes");
 const AdminRoutes = require("./Routes/AdminRoutes");
 const FacultySchema = require("./Routes/FacultyRoutes");
@@ -12,18 +11,16 @@ const ApproveStudents = require("./Routes/ApproveStudents");
 const ApproveFaculty = require("./Routes/ApproveFaculty");
 const PendingFaculty = require("./Routes/PendingFaculty");
 const AssignSubject = require("./Routes/AssignSubject");
+const ExamResult = require("./Routes/ExamResult");
 const GetExams = require("./Routes/GetExams");
 const AddExam = require("./Routes/AddExam");
 const bodyParser = require("body-parser");
-const csvtojson = require("csvtojson");
 const app = express();
-const upload = multer();
 app.use(bodyParser.json());
-const storage = multer.memoryStorage();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
-app.use("/user", UserRoutes, GetExams);
+app.use("/user", UserRoutes, GetExams, ExamResult);
 app.use("/admin", AdminRoutes, AssignSubject);
 app.use("/faculty", PendingFaculty, FacultySchema, AddExam);
 
@@ -56,56 +53,4 @@ mongoose
 // Test Route
 app.get("/", (req, res) => {
   res.send("EndPoints And Backend Working");
-});
-
-const myDataSchema = new mongoose.Schema({
-  name: [
-    {
-      name: { type: String, required: true },
-      age: { type: String, required: true },
-    },
-  ],
-});
-
-const MyData = mongoose.model("MyData", myDataSchema);
-
-// API endpoint for CSV to MongoDB
-app.post("/api/csv-to-mongo", async (req, res) => {
-  // console.log(data);
-  try {
-    const data = new MyData({
-      name: req.body,
-    });
-    data.save((err, savedData) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(savedData);
-      }
-    });
-    const results = await MyData.find();
-    console.log(results);
-    // Return the JSON output
-    res.json(results);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-app.get("/api/fetch-data", async (req, res) => {
-  try {
-    const results = await MyData.find({}).exec();
-    console.log(results);
-    // Convert the data to JSON format
-    // const jsonOutput = results.map((result) => ({
-    //   Name: result.name,
-    //   Age: result.age,
-    // }));
-    z;
-    res.json(results);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
 });
