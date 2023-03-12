49350340d387f4672d3cf2b8744041490df2b3a6
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-03-12 20:44:59 +0530

    Completed Exams Rendered

---
+++ my-project/src/Components/Student/CompletedExamInfo.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+const CompletedExamInfo = ({
+  examID,
+  subjectID,
+  subjectName,
+  marks,
+  totalDuration,
+  totalQuestions,
+}) => {
+  return (
+    <div className="bg-white rounded-lg p-8 mb-8 shadow-md mx-4 w-1/3">
+      <h2 className="text-2xl font-bold mb-4">{subjectName}</h2>
+      <div className="flex flex-col">
+        <div className="flex justify-between mb-4">
+          <p className="font-bold">Subject ID:</p>
+          <p>{subjectID}</p>
+        </div>
+        <div className="flex justify-between mb-4">
+          <p className="font-bold">Total marks:</p>
+          <p>{marks}</p>
+        </div>
+        <div className="flex justify-between mb-4">
+          <p className="font-bold">Total duration:</p>
+          <p>{totalDuration} mins</p>
+        </div>
+        <div className="flex justify-between">
+          <p className="font-bold">Total questions:</p>
+          <p>{totalQuestions}</p>
+        </div>
+      </div>
+      <Link className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4 self-center inline-block">
+        Exam Completed
+      </Link>
+    </div>
+  );
+};
+
+export default CompletedExamInfo;
