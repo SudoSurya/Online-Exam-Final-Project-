8148e0509ddf3d583046a97f8de851e7e4e9bc06
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-03-12 14:38:34 +0530

    Student Results Schema Added

---
+++ Backend/Schemas/StudentResults.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const StudentResults = new mongoose.Schema({
+  userID: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+  userEmail: {
+    type: String,
+    required: true,
+  },
+  userBranch: {
+    type: String,
+    required: true,
+  },
+  Results: {
+    type: [
+      {
+        SubjectID: { type: String, required: true },
+        SubjectName: { type: String, required: true },
+        totalQuestions: { type: Number, required: true },
+        duration: { type: Number, required: true },
+        timeTaken: { type: Number, required: true },
+        score: { type: Number, required: true },
+      },
+    ],
+    default: [],
+  },
+});
+
+module.exports = mongoose.model("StudentResults", StudentResults);
