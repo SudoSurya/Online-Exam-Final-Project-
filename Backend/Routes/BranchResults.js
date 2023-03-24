af2b282c9ec928b15714a71f85da6a63dbb8db7d
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-03-24 21:40:30 +0530

    Single Student Result Api Based on SubjectName Get Api

--- Backend/Routes/BranchResults.js
+++ Backend/Routes/BranchResults.js
@@ -3,9 +3,17 @@ const StudentResults = require("../Schemas/StudentResults");
 let router = express.Router();
 router.use(express.json());
 
-router.get("/student/branch/subject", async (req, res) => {
+router.get("/:studentID/:branch/:subject", async (req, res) => {
   try {
-    const result = await StudentResults.find();
+    const { branch, studentID, subject } = req.params;
+    const result = await StudentResults.find(
+      {
+        userBranch: branch,
+        userID: studentID,
+        "Results.SubjectName": subject,
+      },
+      { "Results.$": 1 }
+    );
     res.json(result);
   } catch (error) {
     console.log(error);
