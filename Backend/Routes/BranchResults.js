2fdfe5b9d05bb0093fcca9d00ead1c0fe2f26a74
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-03-27 12:08:52 +0530

    Branch Results Api Modified (All Subjects Condition Added)

--- Backend/Routes/BranchResults.js
+++ Backend/Routes/BranchResults.js
@@ -6,14 +6,25 @@ router.use(express.json());
 router.get("/:studentID/:branch/:subject", async (req, res) => {
   try {
     const { branch, studentID, subject } = req.params;
-    const result = await StudentResults.find(
-      {
-        userBranch: branch,
-        userID: studentID,
-        "Results.SubjectName": subject,
-      },
-      { "Results.$": 1 }
-    );
+    let result;
+    if (subject === "all") {
+      result = await StudentResults.find(
+        {
+          userBranch: branch,
+          userID: studentID,
+        },
+        { Results: 1, _id: 0 }
+      );
+    } else {
+      result = await StudentResults.find(
+        {
+          userBranch: branch,
+          userID: studentID,
+          "Results.SubjectName": subject,
+        },
+        { "Results.$": 1, _id: 0 }
+      );
+    }
     res.json(result);
   } catch (error) {
     console.log(error);
